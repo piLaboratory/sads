82cd6a064d7d6a2823e700c27c005cc662187980
piklprado <piklprado@gmail.com>
2014-05-31 19:24:39 -0300

    missing man pages added, minor corrections in the corresponding functions

--- R/fitgs.R
+++ R/fitgs.R
@@ -2,19 +2,17 @@ fitgs <- function(x, trunc, ...){
   if(class(x)!="rad") rad.tab <- rad(x)
   else rad.tab <- x
   y <- rep(rad.tab$rank, rad.tab$abund)
-  N <- sum(rad.tab$abund)
   S <- length(rad.tab$abund)
   if (!missing(trunc)){
     if (min(x)<=trunc) stop("truncation point should be lower than the lowest data value")
     else{
-      LL <- function(k, N, S) -sum(dtrunc("gs", x = y, coef = list( k = k, N = N, S = S),
+      LL <- function(k) -sum(dtrunc("gs", x = y, coef = list( k = k, S = S),
                                           trunc = trunc, log = TRUE))
       }
   }
   if (missing(trunc)){
-    LL <- function(k, N, S) -sum(dgs(y, k, N, S, log = TRUE))
+    LL <- function(k) -sum(dgs(y, k, S, log = TRUE))
   }
-  result <- mle2(LL, start = list(k=0.01, N=N, S=S), data = list(x = y), method = "Brent", lower = 0, upper = 1,
-                     fixed=list(N=N, S=S), ...)
+  result <- mle2(LL, start = list(k=0.01), data = list(x = y), method = "Brent", lower = 1e-16, upper = 1-1e-16, ...)
   new("fitrad", result, rad = "gs", distr = "D", trunc = ifelse(missing(trunc), NaN, trunc), rad.tab=rad.tab)
 }
