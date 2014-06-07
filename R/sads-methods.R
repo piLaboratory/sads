86bab0be5e137e7e2957a8d27594a2ca95042c4b
piklprado <piklprado@gmail.com>
2014-06-06 23:25:48 -0300

    qqsad substituted by generic + methods

--- R/sads-methods.R
+++ R/sads-methods.R
@@ -411,7 +411,7 @@ setMethod("radpred",signature(object="missing", sad="character", rad="missing",
           )
 
           
-## ocatvpred generic functions and methods ###
+## octavpred generic functions and methods ###
 
 setGeneric("octavpred",
 def = function(object, sad, rad, coef, trunc, oct, S, N, ...) standardGeneric("octavpred"))
@@ -603,3 +603,71 @@ setMethod("octavpred", signature(object="missing",sad="character", rad="missing"
             new("octav", data.frame(octave = oct, upper = factor(n), Freq = Y))
           }
           )
+
+## Generic and methods for qqsad
+setMethod("qqsad",
+          signature(x="numeric", sad="character", coef="list",
+                    trunc="numeric", distr="character",
+                    plot="logical", line="logical"),
+          function(x, sad, coef, trunc=NA, distr, plot=TRUE, line=TRUE, ...){
+              x.sorted <- sort(x)
+              S <- length(x)
+              if(distr == "D"){
+                  q <- 1:sum(x)
+                  if(!is.na(trunc)){
+                      if(sad == "ls")
+                          p <- do.call(ptrunc, list(sad, q = q, coef = c(list(N=(sum(x)),coef)), trunc = trunc))
+                      else if(sad == "volkov"|| sad=="mzsm")
+                          p <- do.call(ptrunc, list(sad, q = q, coef = c(list(J=(sum(x)),coef)), trunc = trunc))
+                      else
+                          p <- do.call(ptrunc, list(sad, q = q, coef=coef, trunc=trunc))
+                  }
+                  else{
+                      psad <- get(paste("p", sad, sep=""), mode = "function")
+                      if(sad == "ls")
+                          p <- do.call(psad, c(list(q = q, N = sum(x)), coef))
+                      else if(sad =="volkov"||sad=="mzsm")
+                          p <- do.call(psad, c(list(q = q, J=sum(x)), coef))
+                      else{
+                          p <- do.call(psad, c(list(q = q), coef))
+                      }
+                  }
+                  f1 <- approxfun(x=c(1, p), y=c(0, q), method="constant")
+                  q <- f1(ppoints(S))
+              }
+              else if(distr == "C"){
+                  p <- ppoints(S)
+                  if(!is.na(trunc))
+                      q <- do.call(qtrunc, list(sad, p = p, trunc = trunc, coef=coef))
+                  else{
+                      qsad <- get(paste("q", sad, sep=""), mode = "function")
+                      q <- do.call(qsad, c(list(p = p), coef))
+                  }
+              }
+              else
+                  stop("please choose 'D'iscrete or 'Continuous' for 'distr'")
+              if(plot){
+                  dots <- list(...)
+                  if(!"main" %in% names(dots)) dots$main = "Q-Q plot"
+                  if(!"xlab" %in% names(dots)) dots$xlab = "Theoretical Quantile"
+                  if(!"ylab" %in% names(dots)) dots$ylab = "Sample Quantiles"
+                  do.call(graphics::plot, c(list(x=q, y=x.sorted),dots))
+                  if(line) abline(0, 1, col = "red", lty = 2)
+              }
+              return(invisible(data.frame(theoret.q=q, sample.q=x.sorted)))
+          }
+          )
+
+setMethod("qqsad",
+          signature(x="fitsad", sad="ANY", coef="ANY",
+                    trunc="numeric", distr="ANY",
+                    plot="logical", line="logical"),
+          function(object, plot=TRUE, line=TRUE, ...){
+              sad <- object@sad
+              coef <- as.list(bbmle::coef(object))
+              trunc <- object@trunc
+              distr <- object@distr
+              x <- object@data$x
+              qqsad(x=x, sad=sad, coef=coef, trunc=trunc, distr=distr, plot=plot, line=line, ...)
+          }
+)
