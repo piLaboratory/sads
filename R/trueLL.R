098f8054faa5f9b50b0a60666ad8ad68d7fbe61c
piklprado <piklprado@gmail.com>
2014-11-01 13:56:09 -0200

    trueLL now accepts count tables and minor corrections in dvolkov man page

--- R/trueLL.R
+++ R/trueLL.R
@@ -1,14 +1,28 @@
-trueLL <- function(x, dens, coef, trunc, dec.places = 0, log = TRUE, ...){
-  dots <- list(...)
-  D <- 10^(-dec.places)/2
-  x <- round(x, dec.places)
-  if(missing(trunc)){
-    cdf <- get(paste("p", dens, sep=""), mode = "function")
-    probs <- do.call(cdf, c(list(q = x+D), as.list(coef), dots)) - do.call(cdf, c(list(q = x-D), as.list(coef), dots))
-  }
-  else{
-    probs <- do.call(ptrunc, c(list(dens, q = x+D, coef = as.list(coef), trunc=trunc), dots))-
-    do.call(ptrunc, c(list(dens, q = x-D, coef = as.list(coef), trunc=trunc), dots))
-  }
-  if(log) sum(log(probs)) else prod(probs)
+trueLL <- function(x, dens, coef, trunc, dec.places = 0, breaks, counts, log = TRUE, ...){
+    dots <- list(...)
+    if(missing(breaks)){
+        D <- 10^(-dec.places)/2
+        x <- round(x, dec.places)
+        if(missing(trunc)){
+            cdf <- get(paste("p", dens, sep=""), mode = "function")
+            probs <- do.call(cdf, c(list(q = x+D), as.list(coef), dots)) - do.call(cdf, c(list(q = x-D), as.list(coef), dots))
+        }
+        else{
+            probs <- do.call(ptrunc, c(list(dens, q = x+D, coef = as.list(coef), trunc=trunc), dots))-
+                do.call(ptrunc, c(list(dens, q = x-D, coef = as.list(coef), trunc=trunc), dots))
+        }
+    }
+    else{
+        h1 <- hist(x, breaks=breaks, plot=FALSE)
+        if(missing(trunc)){
+            cdf <- get(paste("p", dens, sep=""), mode = "function")
+            probs <- diff(do.call(cdf, c(list(q = h1$breaks), as.list(coef), dots)))
+        }
+        else{
+            probs <- diff(do.call(ptrunc, c(list(dens, q = h1$breaks, coef = as.list(coef), trunc=trunc), dots)))
+        }
+        if(missing(counts)) counts <- h1$counts
+        probs <- rep(probs, counts)
+    }
+    if(log) sum(log(probs)) else prod(probs)
 }
