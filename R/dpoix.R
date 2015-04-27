636a1e19dc59df27da5509ec023e2617da336bec
Andre Chalom <andrechalom@gmail.com>
2015-04-27 18:44:38 -0300

    Some improvements to dpoix and dpoig

--- R/dpoix.R
+++ R/dpoix.R
@@ -1,19 +1,11 @@
 dpoix <- function(x, frac, rate, log=FALSE) {
-    is.wholenumber <- function(x, tol = .Machine$double.eps^0.5){
-      abs(x - round(x)) < tol
-    }
-    if(sum(x,is.wholenumber(x))<length(x))
-       stop("dpoix is a discrete PDF; all y's must be integers")
-    else {
-      f <- function(y){
-        b <- y*log(frac)
-        m <- log(rate)
-        n <- (y+1)*log(rate+frac)
-        exp(b+m-n)
-      }
-      samp <- f(x)
-      if(!log)samp else log(samp)
-    }
-  }
-       
-    
+	if(any(!is.wholenumber(x)))
+		stop("dpoix is a discrete PDF; all x's must be integers")
+	b <- x*log(frac)
+	m <- log(rate)
+	n <- (x+1)*log(rate+frac)
+	vals <- exp(b+m-n)
+	if(log) log(vals) else vals
+}
+
+
