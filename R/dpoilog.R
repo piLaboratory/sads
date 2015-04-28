12aaf7977d864edf839087ba0b6097f0697a10a6
Andre Chalom <andrechalom@gmail.com>
2015-04-28 18:42:28 -0300

    invalid params result in NaN, not error

--- R/dpoilog.R
+++ R/dpoilog.R
@@ -1,9 +1,9 @@
 dpoilog <- function(x, mu, sig, log = FALSE){
-  if (!all(is.finite(c(mu, sig)))) stop("all parameters should be finite")
-  if (sig <= 0) stop("sig must be larger than zero")
-  if (any(x < 0)) stop("at least one x less than zero")
-  if (any(!is.wholenumber(x))) stop("all x must be integers")
-  y <- poilog::dpoilog(x, mu, sig)
-  if (log) return(log(y))
-  else return(y)
+	x[ ! is.wholenumber(x) | x < 0] <- NaN
+	mu[ !is.finite(mu) ] <- NaN
+	sig[ !is.finite(sig) | sig <= 0] <- NaN
+	y <- poilog::dpoilog(x, mu, sig)
+	if (any(is.nan(y))) warning ("NaNs produced")
+	if (log) return(log(y))
+	else return(y)
 }
--- R/dvolkov.R
+++ R/dvolkov.R
@@ -1,12 +1,10 @@
 dvolkov <- function(x, theta, m, J, log=FALSE){
-  if (theta <= 0 || J <= 0 || m < 0 || m > 1){
-    warning("Function not defined for theta or J <= zero, m <0 or m > 1 \n NaN's returned")
-    vals <- rep(NaN, length(x))
-  }
-  else{
-    vals <- volkov(J,c(theta,m))
-    Stot <- sum(vals, na.rm=T)
-  }
-  if(log)log(vals[x]/Stot)
-  else vals[x]/Stot
+	theta[ !is.finite(theta) | theta <= 0] <- NaN
+	J[ !is.finite(J) | J <= 0] <- NaN
+	m[ m < 0 | m > 1 ] <- NaN
+	vals <- volkov(J,c(theta,m))
+	Stot <- sum(vals, na.rm=T)
+	if (any(is.nan(y))) warning ("NaNs produced")
+	if(log)log(vals[x]/Stot)
+	else vals[x]/Stot
 }
