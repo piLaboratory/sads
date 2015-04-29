edf495a76c725bd8c4586062b1eba595d1d8dda6
Andre Chalom <andrechalom@gmail.com>
2015-04-29 17:06:21 -0300

    fixing error in volkov

--- R/dvolkov.R
+++ R/dvolkov.R
@@ -4,7 +4,8 @@ dvolkov <- function(x, theta, m, J, log=FALSE){
 	m[ m < 0 | m > 1 ] <- NaN
 	vals <- volkov(J,c(theta,m))
 	Stot <- sum(vals, na.rm=T)
+	y <- vals[x]/Stot
 	if (any(is.nan(y))) warning ("NaNs produced")
-	if(log)log(vals[x]/Stot)
-	else vals[x]/Stot
+	if(log) return(log(y))
+	else return(y)
 }
