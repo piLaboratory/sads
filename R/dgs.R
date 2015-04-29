16d652d4c71d131cdb9135f3efe8929970e191ad
Andre Chalom <andrechalom@gmail.com>
2015-04-29 16:07:50 -0300

    created function cumsumW to facilitate [p] functions

--- R/dgs.R
+++ R/dgs.R
@@ -1,6 +1,6 @@
 dgs <- function(x, k, S,  log = FALSE) {
 	x[ ! is.wholenumber(x) | x < 1 | x > S ] <- NaN
-	S[ !is.finite(S) | S < 1 | !is.whole.number(S) ] <- NaN
+	S[ !is.finite(S) | S < 1 | !is.wholenumber(S) ] <- NaN
 	k[ k<0 | k>1 ] <- NaN
 	cf <- 1/(1-(1-k)^S)
 	y <- cf*k*(1-k)^(x-1)
--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,23 @@
 is.wholenumber	<-function(x, tol = .Machine$double.eps^0.5){
   abs(x - round(x)) < tol
 }
+
+# Wrapper for cumsum, implementing some
+# error checking, converting NA to NaN for 
+# coherence with [dpqr] functions and
+# padding with 0 for 1-based distributions
+# such as gs
+cumsumW <- function(f, q, coef, lower.tail, log.p, pad) {
+	if (any(sapply(coef, length) > 1)) stop("Vectorization not implemented for the parameters")
+	dist <- get(paste("d", f, sep=""), mode="function")
+	if (pad)
+		z <- c(NaN, cumsum(do.call(dist, c(list(x=1:max(q)), coef))))
+	else
+		z <- cumsum(do.call(dist, c(list(x=0:max(q)), coef)))
+	y <- z[q+1]
+	y[! is.wholenumber(q) ] <- NaN
+  if(!lower.tail) y <- 1-y
+  if(log.p) y <- log(y)
+	y[is.na(y)] <- NaN
+	return (y)
+}
