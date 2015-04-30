195e4940cac3472398e6c8d103b0949a26bfd954
Andre Chalom <andrechalom@gmail.com>
2015-04-30 19:15:06 -0300

    informative calls for poi[gx]

--- R/fitpoig.R
+++ R/fitpoig.R
@@ -14,12 +14,11 @@ fitpoig <- function(x, trunc = 0, start.value, ...){
     if (min(x)<=trunc) stop("truncation point should be lower than the lowest data value")
     else{
       LL <- function(frac, rate, shape) -sum(dtrunc("poig", x = x, coef = list(frac = frac, rate = rate, shape=shape), trunc = trunc, log = TRUE))
-      result <- mle2(LL, start = list(frac = frac, rate=rate, shape=shape), data = list(x = x), ...)
     }
   }
   if (missing(trunc)){
     LL <- function(frac, rate, shape) -sum(dpoig(x, frac, rate, shape, log = TRUE))
-    result <- mle2(LL, start = list(frac=frac, rate=rate, shape=shape), data = list(x = x), ...)
   }
+  result <- do.call("mle2", c(list(LL, start = list(frac = frac, rate=rate, shape=shape), data = list(x = x)), ...))
   new("fitsad", result, sad = "poig", distr = "D", trunc = ifelse(missing(trunc), NaN, trunc))
 }
--- R/fitpoix.R
+++ R/fitpoix.R
@@ -12,12 +12,11 @@ fitpoix <- function(x, trunc = 0, start.value, ...){
     if (min(x)<=trunc) stop("truncation point should be lower than the lowest data value")
     else{
       LL <- function(frac, rate) -sum(dtrunc("poix", x = x, coef = list(frac = frac, rate = rate), trunc = trunc, log = TRUE))
-      result <- mle2(LL, start = list(frac = frac, rate=rate), data = list(x = x), ...)
     }
   }
   if (missing(trunc)){
     LL <- function(frac, rate) -sum(dpoix(x, frac, rate, log = TRUE))
-    result <- mle2(LL, start = list(frac=frac, rate=rate), data = list(x = x), ...)
   }
+  result <- do.call("mle2", c(list(LL, start = list(frac=frac, rate=rate), data = list(x = x)), ...))
   new("fitsad", result, sad = "poix", distr = "D", trunc = ifelse(missing(trunc), NaN, trunc))
 }
