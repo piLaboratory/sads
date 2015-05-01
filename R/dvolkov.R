dcf96029a2af44b9dab2d5dda454179f400a8798
Andre Chalom <andrechalom@gmail.com>
2015-05-01 18:55:06 -0300

    slightly faster dvolkov

--- R/dvolkov.R
+++ R/dvolkov.R
@@ -1,32 +1,28 @@
 ## Uses code from untb::volkov, by Robin K. S. Hankin
 ## http://cran.r-project.org/web/packages/untb/
 dvolkov <- function(x, theta, m, J, log=FALSE){
-	theta[ !is.finite(theta) | theta <= 0] <- NaN
-	J[ !is.finite(J) | J <= 0] <- NaN
-	m[ m < 0 | m > 1 ] <- NaN
-	vals <- volkov(J,theta,m)
+	if(length(theta) > 1 | length(m) > 1 | length(J) > 1) stop ("Vectorization of parameters not implemented")
+	if(!is.finite(theta) | theta <= 0 | !is.finite(J) | J <= 0 | m <= 0 | m >= 1) return (rep(NaN, length(x)))
+
+	gam <- m*(J-1)/(1-m)
+	# some preliminary calculations to speed up
+	# lJm is the term that only depends on J and m
+	lJm <- lgamma(J+1) + lgamma(gam) - lgamma(J+gam)
+	integrand <- function(y, n, ln){
+		theta * exp(lJm + ln + 
+					lgamma(n+y) + lgamma(J-n+gam-y) - lgamma(1+y) - lgamma(gam-y)-
+					y*theta/gam
+					)
+	}
+	f <- function(n){
+		# ln is the term that only depends on J, m, and n
+		ln <- - lgamma(n+1) - lgamma(J-n+1)
+		integrate(integrand, lower=0, upper=gam, n=n, ln=ln)$value
+	}
+	vals <- sapply(1:J,f)
 	Stot <- sum(vals, na.rm=T)
 	y <- vals[x]/Stot
 	if (any(is.nan(y))) warning ("NaNs produced")
 	if(log) return(log(y))
 	else return(y)
 }
-
-volkov <- function(J, theta, m){
-  gam <- m*(J-1)/(1-m)
-  integrand <- function(y,n){
-    theta*
-      exp(lgamma(J+1) - lgamma(n+1) - lgamma(J-n+1) + 
-          lgamma(gam) - lgamma(J+gam) + 
-          lgamma(n+y) + lgamma(J-n+gam-y) - lgamma(1+y) - lgamma(gam-y)-
-          y*theta/gam
-          )
-  }
-  
-  f <- function(n){
-	  integrate(integrand, lower=0, upper=gam, n=n)$value
-  }
-  out <- sapply(1:J,f)
-  return(out)
-}
-
