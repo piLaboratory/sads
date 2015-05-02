97b8dbe673a4cc913fd1218138fb0411e94c504a
Andre Chalom <andrechalom@gmail.com>
2015-05-01 21:23:30 -0300

    Working volkov.c

--- R/dvolkov.R
+++ R/dvolkov.R
@@ -1,27 +1,10 @@
-## Uses code from untb::volkov, by Robin K. S. Hankin
-## http://cran.r-project.org/web/packages/untb/
 dvolkov <- function(x, theta, m, J, log=FALSE){
 	if(length(theta) > 1 | length(m) > 1 | length(J) > 1) stop ("Vectorization of parameters not implemented")
 	if(!is.finite(theta) | theta <= 0 | !is.finite(J) | J <= 0 | m <= 0 | m >= 1) return (rep(NaN, length(x)))
 
-	vals <- .C(volkov, x0=as.double(x), theta0=as.double(theta), m0=as.double(m), J0=as.integer(J))
-	print(vals)
-	return();
-	gam <- m*(J-1)/(1-m)
-	# some preliminary calculations to speed up
-	# lJm is the term that only depends on J and m
-	lJm <- lgamma(J+1) + lgamma(gam) - lgamma(J+gam)
-	integrand <- function(y, n, ln){
-		exp(ln + lgamma(n+y) + lgamma(J-n+gam-y) - lgamma(1+y) - lgamma(gam-y)- y*theta/gam)
-	}
-	f <- function(n){
-		# ln is the term that only depends on J, m, and n
-		ln <- lJm - lgamma(n+1) - lgamma(J-n+1)
-		theta * integrate(integrand, lower=0, upper=gam, n=n, ln=ln)$value
-	}
-	vals <- sapply(1:J,f)
-	Stot <- sum(vals, na.rm=T)
-	y <- vals[x]/Stot
+	Cvolkov <- .C(volkov, res=as.double(rep(0,J)), theta0=as.double(theta), m0=as.double(m), J0=as.integer(J))
+	y <- Cvolkov$res[x]
+	y[ !is.wholenumber(x) ] <- NaN
 	if (any(is.nan(y))) warning ("NaNs produced")
 	if(log) return(log(y))
 	else return(y)
--- src/volkov.c
+++ src/volkov.c
@@ -1,34 +1,40 @@
 #include <math.h>
 #include <Rmath.h>
 #include <R.h>
-#include <stdio.h>
-#define MAX 20000
 
-/* Calculates the Volkov distribution density */
+/*** Calculates the Volkov distribution density
+     Based on code from untb::volkov, by Robin K. S. Hankin
+     http://cran.r-project.org/web/packages/untb/ */
 
-double f(double y, int n, double ln, int J, double gam, double theta) {
-	return exp(ln + lgamma(n+y) + lgamma(J-n+gam-y) - lgamma(1+y) - lgamma(gam-y)- y*theta/gam);
+double f(double y, int n, double ln, int J, double upper, double theta) {
+	return exp(ln + lgamma(n+y) + lgamma(J-n+upper-y) - lgamma(1+y) - lgamma(upper-y)- y*theta/upper);
 }
 
-double integrate(int n, double lJm, int J, double gam, double theta, double from, double to) {
-	int N = 200; /* hardcoded subdividions, may transform in f parameter later */
-	double h = (to-from)/ (double) N;
+double integrate(int n, double lJm, int J, double upper, double theta) {
+	int N = 5000; /* hardcoded subdividions, may transform in f parameter later */
+	double h = upper/N;
 	double ln = lJm - lgamma(n+1) - lgamma(J-n+1);
-	double part = f(from, n, ln, J, gam, theta) + f(to, n, ln, J, gam, theta);
+	/* upper = J causes NaN, slight approximation here */
+	double part = f(0, n, ln, J, upper, theta) + f(upper-1.0/N/N, n, ln, J, upper, theta);
 	for (int i = 1; i < N; i+=2)
-		part += 4.0 * f(from + i*h, n, ln, J, gam, theta);
+		part += 4.0 * f(i*h, n, ln, J, upper, theta);
 	for (int i = 2; i < N; i+=2)
-		part += 2.0 * f(from + i*h, n, ln, J, gam, theta);
+		part += 2.0 * f(i*h, n, ln, J, upper, theta);
 	return part * h / 3.0;
 }
 
 extern
-void volkov ( double * x0, double * theta0, double * m0, int * J0) {
+void volkov ( double * res, double * theta0, double * m0, int * J0) {
 	int J = J0[0];
-	double theta = theta0[0], m = m0[0], gam = m*(J-1)/(1-m);
+	double theta = theta0[0], m = m0[0], upper = m*(J-1)/(1-m), total = 0;
 	/* some preliminary calculations to speed up
 	   lJm is the term that only depends on J and m */
-	double lJm = lgamma(J+1) + lgamma(gam) - lgamma(J+gam);
-	x0[0] = theta * integrate(1, lJm, J, gam, theta, 0, 1);
+	double lJm = lgamma(J+1) + lgamma(upper) - lgamma(J+upper);
+	for (int i = 0; i < J; i++) {
+		res[i] = theta * integrate(i+1, lJm, J, upper, theta);
+		total += res[i];
+	}
+	for (int i = 0; i < J; i++) {
+		res[i] /= total;
+	}
 }
-
