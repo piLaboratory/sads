7d638a88538234b3c74b271dcf04f20988cf31de
Andre Chalom <andrechalom@gmail.com>
2015-05-05 18:38:52 -0300

    fixing #8 for poi[gx]

--- R/dpoig.R
+++ R/dpoig.R
@@ -1,5 +1,4 @@
 dpoig <- function(x, frac, rate, shape, log=FALSE) {
-	x[ ! is.wholenumber(x) | x < 0 ] <- NaN
 	frac[ !is.finite(frac) | frac <= 0 ] <- NaN
 	rate[ !is.finite(rate) | rate <= 0 ] <- NaN
 	shape[ !is.finite(shape) | shape <= 0 ] <- NaN
@@ -7,5 +6,7 @@ dpoig <- function(x, frac, rate, shape, log=FALSE) {
 	c <- lfactorial(x)+lgamma(shape)+(x+shape)*log(frac+rate)
 	vals<-b-c
 	if (any(is.nan(vals))) warning ("NaNs produced")
+	if (any(!is.wholenumber(x))) warning("non integer values in x")
+	vals[ ! is.wholenumber(x) | x < 0] <- -Inf
 	if(log)vals else exp(vals)
 }
