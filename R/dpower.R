0fbb4302377487f9a1a442b9c5685a3beab52ea2
Andre Chalom <andrechalom@gmail.com>
2015-05-05 15:51:53 -0300

    [d] functions return 0 instead of NaN for invalid x

--- R/dpower.R
+++ R/dpower.R
@@ -1,8 +1,9 @@
 dpower <- function(x, s, log = FALSE){
-	x[ ! is.wholenumber(x) | x < 1 ] <- NaN
 	s[ !is.finite(s) | s <= 1 ] <- NaN
 	y <- -s*log(x)-log(zeta(s))
 	if (any(is.nan(y))) warning ("NaNs produced")
+	if (any(!is.wholenumber(x))) warning("non integer values in x")
+	y[ ! is.wholenumber(x) | x < 1] <- 0
 	if(log) return(y)
 	else return(exp(y))
 }
--- R/dvolkov.R
+++ R/dvolkov.R
@@ -2,10 +2,13 @@ dvolkov <- function(x, theta, m, J, log=FALSE){
 	theta[ !is.finite(theta) | theta <= 0] <- NaN
 	J[ !is.finite(J) | J <= 0] <- NaN
 	m[ m < 0 | m > 1 ] <- NaN
-	vals <- volkov(J,c(theta,m))
+	if (any(!is.wholenumber(x))) warning("non integer values in x")
+	x[ ! is.wholenumber(x) | x < 0] <- 0
+	vals <- c(0, volkov(J,c(theta,m))) # index 0 should receive 0
 	Stot <- sum(vals, na.rm=T)
-	y <- vals[x]/Stot
+	y <- vals[x+1]/Stot
 	if (any(is.nan(y))) warning ("NaNs produced")
+	if (any(!is.wholenumber(x))) warning("non integer values in x")
 	if(log) return(log(y))
 	else return(y)
 }
