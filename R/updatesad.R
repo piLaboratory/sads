9a0e2e73b806e57c1fe54abc4f6f4499d99d61cd
Andre Chalom <andrechalom@gmail.com>
2015-05-22 02:08:41 -0300

    updatesad now works for models with fixed params

--- R/updatesad.R
+++ R/updatesad.R
@@ -1,7 +1,7 @@
 ## updates a fitsad/fitrad object by running the optimizer again starting
 ## on better fit returned by profile
 updatesad <- function(object, ...) {
-	dots <- as.list(...)
+	dots <- list(...)
 	prof <- profile(object)
 	if(class(prof) == "profile.mle2") stop("Cannot update, profile did not find a better fit!")
 	newcall <- as.list(object@call)
@@ -9,9 +9,15 @@ updatesad <- function(object, ...) {
 	newcall[["control"]] <- NULL # removes the "control" slot
 	# merges the original call with arguments supplied by ...
 	for (v in names(dots)) newcall[[v]] <- dots[[v]]
-	names(newcall$start) -> name # bugfix? profile returns coefficient "prob" name as "prob.prob"
-	newcall$start <- list(prof@fullcoef) # TODO: might be coef instead? how to deal with fixed?
-	names(newcall$start) <- name
+	# bugfix? profile returns coefficient "prob" name as "prob.prob", sometimes changes parameters
+	# from start to fixed...
+	names(newcall$start) -> name 
+	for (v in name) {
+		if (v %in% names(prof@coef))
+			newcall$start[[v]] <- as.numeric(prof@coef[[v]])
+		else # then it must be in fixed
+			newcall$start[[v]] <- as.numeric(prof@call.orig$fixed[v])
+	}
 	newobj <- do.call("mle2", newcall)
 	if(class(object) == "fitsad") 
 		return (new("fitsad", newobj, sad=object@sad, distr=object@distr, trunc=object@trunc))
