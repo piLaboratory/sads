3d0532d0876a16aa4382edd4faa7a70465760826
Andre Chalom <andrechalom@gmail.com>
2015-05-22 18:59:13 -0300

    Added more signatures for trueLL, updated man
    Added a warning on the man page for the weird results.
    Also, removed conflicting definitions of trueLL found in the
    sads-methods.R
    
    NOTE: pkg install throws a warning about the siglist for
    trueLL("histogram", ...) because histogram is not a valid S4 class,
    but the code works.

--- R/trueLL.R
+++ R/trueLL.R
@@ -1,35 +1,54 @@
 # trueLL generic and methods
 setGeneric("trueLL", 
-		   def=function(object, dist, coef, trunc, dec.places, breaks, counts, log = TRUE, ...)
+		   def=function(object, dist, coef, trunc, dec.places=0, breaks, ...)
 		   standardGeneric("trueLL")
 		   )
 setMethod("trueLL", signature(object="fitsad", dist="missing", coef="missing", trunc="missing", 
-							  dec.places="ANY", breaks="ANY", counts="ANY", log="ANY"),
-		  function(object, dist, coef, trunc, dec.places, breaks, counts, log = TRUE, ...){
-			  trueLL(object=object@data$x, dist=object@sad, coef=as.list(bbmle::coef(object)),
-					 trunc=object@trunc, dec.places=dec.places, breaks=breaks, counts=counts, log=log, ...)
+							  dec.places="ANY", breaks="ANY"),
+		  function(object, dec.places=0, breaks, ...){
+			  if(missing(breaks))
+					  return(trueLL(object=object@data$x, dist=object@sad, coef=as.list(bbmle::coef(object)),
+							 trunc=object@trunc, dec.places=dec.places, ...))
+			  else 
+					  return(trueLL(object=object@data$x, dist=object@sad, coef=as.list(bbmle::coef(object)),
+							 trunc=object@trunc, breaks=breaks, ...))
 		  })
-# Default invocation, object is numeric
+setMethod("trueLL", signature(object="fitrad", dist="missing", coef="missing", trunc="missing", 
+							  dec.places="ANY", breaks="ANY"),
+		  function(object, dec.places=0, breaks, ...){
+			  if(missing(breaks))
+				  return(trueLL(object=object@rad.tab$abund, dist=object@rad, coef=as.list(bbmle::coef(object)),
+						 trunc=object@trunc, dec.places=dec.places, breaks=breaks, ...))
+			  else
+				  return(trueLL(object=object@rad.tab$abund, dist=object@rad, coef=as.list(bbmle::coef(object)),
+						 trunc=object@trunc, breaks=breaks, ...))
+		  })
+# object is numeric
 setMethod("trueLL", signature(object="numeric", dist="character", coef="list", trunc="ANY", 
-							  dec.places="ANY", breaks="ANY", counts="ANY", log="ANY"),
-		  function(object, dist, coef, trunc, dec.places, breaks, counts, log = TRUE, ...){
+							  dec.places="ANY", breaks="ANY"),
+		  function(object, dist, coef, trunc, dec.places=0, breaks, ...){
 			  dots <- list(...)
-			  if(! missing(breaks) & ! missing(dec.places)) 
-				  stop("Unable to provide both dec.places and breaks")
 			  if(missing(breaks)){
-				  if(missing(dec.places)) dec.places=0
 				  D <- 10^(-dec.places)/2
 				  breaks <- sort(unique(c(object - D, object + D)))
 			  }
 			  h1 <- hist(object, breaks=breaks, plot=FALSE)
-			  if(missing(trunc) | is.nan(trunc)){
+			  if(missing(trunc)) trunc <- NaN
+			  trueLL(object=h1, dist=dist, coef=coef, trunc=trunc)
+		  })
+# workhorse method, all other methods redirect here
+setMethod("trueLL", signature(object="list", dist="character", coef="list", trunc="ANY", 
+							  dec.places="missing", breaks="missing"),
+		  function(object, dist, coef, trunc, ...){
+			  dots <- list(...)
+			  if(missing(trunc)) trunc <- NaN
+			  if(is.nan(trunc)){
 				  cdf <- get(paste("p", dist, sep=""), mode = "function")
-				  probs <- diff(do.call(cdf, c(list(q = h1$breaks), as.list(coef), dots)))
+				  probs <- diff(do.call(cdf, c(list(q = object$breaks), as.list(coef), dots)))
 			  }
 			  else{
-				  probs <- diff(do.call(ptrunc, c(list(dist, q = h1$breaks, coef = as.list(coef), trunc=trunc), dots)))
+				  probs <- diff(do.call(ptrunc, c(list(dist, q = object$breaks, coef = as.list(coef), trunc=trunc), dots)))
 			  }
-			  if(missing(counts)) counts <- h1$counts
-			  probs <- rep(probs, counts)
-			  if(log) sum(log(probs)) else prod(probs)
+			  probs <- rep(probs, object$counts)
+			  return(sum(log(probs)))
 		  })
