1ecf6e2b9b86259cb6e36eb5fe7e87df2d8c1fbf
piklprado <piklprado@gmail.com>
2015-09-11 02:28:11 -0300

    Fitting issue #69 - Inconsistent calls between fitgeom and fitsad
    The bug probably was caused by overflow of the likelihood function for extreme values. Apparently Brent's ethod could not handle the problem. I changed the method optmization method for the mle2 defaults.
    
    Meanwhile, I realized that fitting functions that do zero-truncation by default (dgeom, dnbinom and dpoilog) were not allowing users to include zeroes. Fixed.

--- R/fitsad.R
+++ R/fitsad.R
@@ -1,16 +1,14 @@
-fitsad <- function(x, sad=c("bs","gamma","geom","lnorm","ls","mzsm","nbinom","pareto","poilog","power","weibull","volkov", "poig", "poix"), ...){ 
+fitsad <- function(x, sad=c("bs","gamma","geom","lnorm","ls","mzsm","nbinom","pareto","poilog","power","volkov", "weibull"), ...){ 
   dots <- list(...)
   sad <- match.arg(sad)
   fit <- get(paste("fit", sad, sep=""), mode = "function")
-  if(!"trunc" %in% names(dots) && (sad %in% c("poilog", "geom", "nbinom", "poig", "poix"))) {
+  if(!"trunc" %in% names(dots) && (sad %in% c("poilog", "geom", "nbinom"))) {
     #BUGFIX: some objects (ie, those created with zeroes=T) have some species with 0 individuals
-    # we remove those species before fitting
+    # we remove those species before fitting to avoid zero-truncated fits throwing an error
     if (sum(x==0) > 0) {
       warning("Removing zeroes from abundance vector")
       x <- x[x > 0]
-    }   
-    do.call(fit, c(list(x = x, trunc = 0), dots))
   }
-  else
-    do.call(fit, c(list(x = x), dots))
+}
+  do.call(fit, c(list(x = x), dots))
 }
