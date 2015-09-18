82e18683dabe616ca711b02e7222723cc56c6d85
Andre Chalom <andrechalom@gmail.com>
2015-09-18 18:32:02 -0300

    Suppressing warnings on dmzsm
    The changes to dmzsm by @piklprado were throwing a warning about NaN
    values being generated when x > J, even with the final result being 0.

--- R/dmzsm.R
+++ R/dmzsm.R
@@ -4,7 +4,7 @@ dmzsm <- function(x, J, theta, log = FALSE){
 	mzsm <- function(y, J, theta) (theta/y)*(1-y/J)^(theta-1)
 	sn <- mzsm(y=x, J = J, theta = theta)
 	mu <- mzsm(y=1:J, J = J, theta = theta)
-	lpn <- log(sn) - log(sum(mu))
+	lpn <- suppressWarnings(log(sn) - log(sum(mu))) # Might be NaN if x > 0, but end result will be zero
         lpn[ x <= 0 | x > J ] <- -Inf
 	if (any(is.nan(lpn))) warning ("NaNs produced")
 	if(log) return(lpn)
