842c6089d9032d5cab468608c06a34f456234e53
Andre Chalom <andrechalom@gmail.com>
2015-10-09 18:46:39 -0300

    Adding distr function

---
+++ R/distr.R
@@ -0,0 +1,20 @@
+#' Continuous or discrete distributions
+#'
+#' Checks if the distribution is continuous or discrete
+#'
+#' Returns whether a given distribution (used for a sad or rad model) is "discrete"
+#' or "continuous". The name is compared to a list of known distributions, so distributions
+#' not used in the sads package will return "NA" with a warning.
+#' 
+#' @param distribution Character. The name of the distribution ("geom" for "fitgeom", "weibull" for "fitweibull", etc.
+distr <- function(distribution) {
+  if (class(distribution)!="character") stop("Distribution must be from class character")
+  if (distribution %in% c("bs", "lnorm", "gamma", "pareto", "weibull"))
+    return("C")
+  if (distribution %in% c("gs", "geom", "rbs", "power", "poilog", "nbinom", "mzsm", "mand", 
+                          "ls", "volkov", "zipf"))
+    return("D")
+  # if arrived here...
+  warning(paste("Unknown distribution:", distribution))
+  return(NA)
+}
