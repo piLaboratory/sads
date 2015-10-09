585d02772b7caefb485531d0bf2b0d9baa612a78
Andre Chalom <andrechalom@gmail.com>
2015-10-09 20:11:56 -0300

    Updating docs about distr deprecation

--- R/distr.R
+++ R/distr.R
@@ -6,6 +6,10 @@
 #' or "continuous". The name is compared to a list of known distributions, so distributions
 #' not used in the sads package will return "NA" with a warning.
 #' 
+#' In the package sads up to version 0.2.3, the user was required to explicitly set 
+#' a distr argument in some calls to \code{\link{radpred}} and \code{\link{qqsad}}. Now
+#' this is handled automatically, and attempts to set the "distr" argument explicitly are ignored.
+#' 
 #' @param distribution Character. The name of the distribution ("geom" for "fitgeom", "weibull" for "fitweibull", etc.
 distr <- function(distribution) {
   if (class(distribution)!="character") stop("Distribution must be from class character")
