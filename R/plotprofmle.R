e9392ce92d76b5fb5b381cb7bd3986aab7da409b
Andre Chalom <andrechalom@gmail.com>
2015-10-14 20:20:58 -0300

    INTERNAL

--- R/plotprofmle.R
+++ R/plotprofmle.R
@@ -34,37 +34,19 @@ function(object, nseg, ratio, which, ask, col.line, varname, ...){
   }
   for(i in parseq)
     {
-      tmp <- mleprof[i][[1]]
-      y <- tmp[,1]^2/2
-      x <- (tmp[,2][,i])
-      interpolF = splinefun(x, y, method="monoH.FC")
-      # Redo the x axis to increase the number of points in nseg times
-      l = nseg*length(x); a <- (max(x) - min(x))/(l-1)
-      x <- a * 1:l + min(x)-a
-      y <- interpolF(x)
+      prof <- internal.spline(mleprof, i, ratio)
 
-      do.call(plot, c(list(x=x, y=y, xlab=vname[i]),dots))
-      if(!is.null(ratio)){
-        l <- length(y)
-	  # Finds where the ation crosses the "y = ratio" line
-        change <- (y - ratio)[2:l] * (y - ratio)[1:(l-1)]
-        endpoints <- which(change < 0)
-		# Adds the borders, if any of them is lower than ratio
-		if(y[1] < ratio) endpoints <- c(1, endpoints)
-		if(y[l] < ratio) endpoints <- c(endpoints, l)
-        corr <- (x[2]-x[1])/2
-		if(length(endpoints) > 0)
-        for (j in 1:(length(endpoints)/2)) {
-          lower <-x[endpoints[(2*j)-1]]+corr
-          upper <-x[endpoints[2*j]]+corr
+      do.call(plot, c(list(x=prof$x, y=prof$y, xlab=vname[i]),dots))
+		if(length(prof$endpoints) > 0)
+        for (j in 1:(length(prof$endpoints)/2)) {
+          lower <-x[prof$endpoints[(2*j)-1]]+corr
+          upper <-x[prof$endpoints[2*j]]+corr
           lines(c(lower,upper ),c(ratio, ratio), col=col.line, lty=2)
-		  if(endpoints[(2*j-1)] != 1) # dont draw vertical lines at the borders
-	          lines(rep(lower,2), c(-1, ratio), col=col.line, lty=2)
-		  if(endpoints[(2*j)] != l) # dont draw vertical lines at the borders
-			  lines(rep(upper,2), c(-1, ratio), col=col.line, lty=2)
+          if(prof$endpoints[(2*j-1)] != 1) # dont draw vertical lines at the borders
+            lines(rep(lower,2), c(-1, ratio), col=col.line, lty=2)
+          if(prof$endpoints[(2*j)] != l) # dont draw vertical lines at the borders
+            lines(rep(upper,2), c(-1, ratio), col=col.line, lty=2)
         }
-      }
-
     }
 })
 setMethod("plotprofmle", "mle2",
@@ -73,3 +55,28 @@ setMethod("plotprofmle", "mle2",
     cat("in a different variable\n")
     plotprofmle(profile(object), ...)
 })
+
+internal.spline <- function(mleprof, i, ratio) {
+      tmp <- mleprof[i][[1]]
+      y <- tmp[,1]^2/2
+      x <- (tmp[,2][,i])
+      interpolF = splinefun(x, y, method="monoH.FC")
+      # Redo the x axis to increase the number of points in nseg times
+      l = nseg*length(x); a <- (max(x) - min(x))/(l-1)
+      x <- a * 1:l + min(x)-a
+      y <- interpolF(x)
+      lower <- c()
+      upper <- c()
+      if(!is.null(ratio)){
+        l <- length(y)
+        # Finds where the ation crosses the "y = ratio" line
+        change <- (y - ratio)[2:l] * (y - ratio)[1:(l-1)]
+        endpoints <- which(change < 0)
+        # Adds the borders, if any of them is lower than ratio
+        if(y[1] < ratio) endpoints <- c(1, endpoints)
+        if(y[l] < ratio) endpoints <- c(endpoints, l)
+        corr <- (x[2]-x[1])/2
+      }
+      return(list(x=x,y=y,endpoints=endpoints))
+}
+
