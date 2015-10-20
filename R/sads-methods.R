d7b08f526203e1268a1d687575c021ff3eb2ec8b
Andre Chalom <andrechalom@gmail.com>
2015-10-19 22:09:10 -0200

    Distr is deprecated in octavpred

--- R/sads-methods.R
+++ R/sads-methods.R
@@ -391,7 +391,7 @@ setMethod("octavpred", signature(object="missing",sad="character", rad="missing"
             oct <- unique(oct)
             if (preston) {
               if(missing(trunc)) trunc <- NaN
-              return(octav(radpred(sad=sad, coef=coef, trunc=trunc, distr="D", S=S, N=N)$abund, preston=TRUE))
+              return(octav(radpred(sad=sad, coef=coef, trunc=trunc, distr=distr.depr, S=S, N=N)$abund, preston=TRUE))
             } else {
               n <- 2^oct
               if(!missing(trunc) & !is.nan(trunc)){
