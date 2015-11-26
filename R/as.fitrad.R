ace1e305b1f8510138bc7002e72751462bf07b86
Andre Chalom <andrechalom@gmail.com>
2015-11-26 17:22:17 -0200

    Working?

--- R/as.fitrad.R
+++ R/as.fitrad.R
@@ -1,3 +1,32 @@
+#' fitsad to fitrad conversion
+#' 
+#' Converts a \code{\link{fitsad}} object into a \code{\link{fitrad}} object, for comparisons
+#'
+#' This function takes a fitsad object and generates the corresponding fitrad, in order to enable
+#' comparisons such as the AIC.
+#' @param object A \code{link{fitsad}} object to be converted
+#' @examples
+#' f1 <- fitlnorm(moths)
+#' logLik(f1)
+#' # This is incomparable with fitrad objects, such as
+#' fz <- fitzipf(moths)
+#' logLik(fz)
+#' # However, the converted fitrad has a comparable log-likelihood
+#' f2 <- as.fitrad(f1)
+#' logLik(f2)
+as.fitrad <- function(object) {
+  if(!inherits(object, "fitsad")) stop("object should be a fitsad")
+  if (object@sad == "lnorm") {
+    LL <- function(meanlog, sdlog) -sum(drad(object@sad, object@data$x, log=TRUE, list(meanlog=meanlog, sdlog=sdlog), NaN))
+    result <- do.call("mle2", list(LL, eval.only=TRUE, start=list(meanlog=object@coef[1], sdlog=object@coef[2])))
+  } else if(object@sad == "ls") {
+    LL <- function(N, alpha) -sum(drad(object@sad, object@data$x, log=TRUE, list(N=N, alpha=alpha), NaN))
+    result <- do.call("mle2", list(LL, eval.only=TRUE, start=list(N=object@fullcoef[1], alpha=object@fullcoef[2])))
+  } else stop ("this distribution has not been implemented yet")
+
+  new("fitrad", result, rad=object@sad, distr = distr.depr, trunc = object@trunc, rad.tab=rad(object@data$x))
+}
+
 drad <- function(f, x, log = FALSE, coef, trunc = NaN) {
   rad.tab <- rad(x)
   y <- rep(rad.tab$rank, rad.tab$abund)
@@ -15,59 +44,3 @@ drad <- function(f, x, log = FALSE, coef, trunc = NaN) {
   else 
     num / sum(den)
 }
-
-drad_lnorm <- function(x, meanlog, sdlog, log = FALSE) drad("lnorm", x, log, list(meanlog=meanlog, sdlog=sdlog))
-drad_ls <- function(x, N, alpha, log = FALSE) drad("ls", x, log, list(N=N, alpha=alpha))
-
-prad <- function () {}
-
-qrad <- function () {}
-
-#' fitsad to fitrad conversion
-#' 
-#' Converts a \code{\link{fitsad}} object into a \code{\link{fitrad}} object, for comparisons
-#'
-#' This function takes a fitsad object and generates the corresponding fitrad, in order to enable
-#' comparisons such as the AIC.
-as.fitrad <- function(object) {
-  if(!inherits(object, "fitsad")) stop("object should be a fitsad")
-  if(!is.nan(object@trunc)) stop("truncated objects are not allowed in this version")
-  if (object@sad == "lnorm") {
-    LL <- function(meanlog, sdlog) -sum(drad(object@sad, object@data$x, log=TRUE, list(meanlog=meanlog, sdlog=sdlog), NaN))
-    result <- do.call("mle2", list(LL, start=list(meanlog=object@coef[1], sdlog=object@coef[2])))
-  } else if(object@sad == "ls") {
-    LL <- function(N, alpha) -sum(drad(object@sad, object@data$x, log=TRUE, list(N=N, alpha=alpha), NaN))
-    result <- do.call("mle2", list(LL, start=list(N=object@fullcoef[1], alpha=object@fullcoef[2])))
-  } else stop ("this distribution has not been implemented yet")
-
-  new("fitrad", result, rad=paste0("rad_", object@sad), distr = distr.depr, trunc = object@trunc, rad.tab=rad(object@data$x))
-}
-
-#fitmand <- function(x, trunc, start.value, ...){
-#  if(class(x)!="rad") rad.tab <- rad(x)
-#  else rad.tab <- x
-#  N <- max(rad.tab$rank)
-#  y <- rep(rad.tab$rank, rad.tab$abund)
-#  dots <- list(...)
-#  if (!missing(trunc)){
-#    if (min(y)<=trunc) stop("truncation point should be lower than the lowest data value")
-#  }
-#  if(missing(start.value)){
-#	x75 <- rad.tab[(floor(dim(rad.tab)[1]/4)):dim(rad.tab)[1],]
-#    x75 <- x75[x75$abund > 1, ]
-#	shat <- - coef(lm(log(abund)~log(rank), data=x75))[2]
-#    vhat <- 30
-#  }
-#  else{
-#    shat <- start.value[1]
-#    vhat <- start.value[2]
-#  }
-#  if (missing(trunc)){
-#    LL <- function(N, s, v) -sum(dmand(y, N = N, s=s, v=v, log = TRUE))
-#  }
-#  else{
-#    LL <- function(N, s, v) -sum(dtrunc("mand", x = y, coef = list(N = N, s = s, v = v), trunc = trunc, log = TRUE))
-#  }
-#  result <- do.call("mle2", c(list(LL, start = list(s = shat, v = vhat), fixed=list(N=N), data = list(x = y)), dots))
-#  new("fitrad", result, rad="mand", distr = distr.depr, trunc = ifelse(missing(trunc), NaN, trunc), rad.tab=rad.tab)
-#}
