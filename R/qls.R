47f0af780ebe161838ad6d647c4295204b9aaba5
Andre Chalom <andrechalom@gmail.com>
2015-11-30 19:38:51 -0200

    Optimizing qfinder

--- R/qls.R
+++ R/qls.R
@@ -1,9 +1,14 @@
 qls<-function(p, N, alpha, lower.tail = TRUE, log.p = FALSE){
+  if(any(p != sort(p))) stop("p vector should be in ascending order")
   if(length(N) > 1 | length(alpha) > 1) stop("Vectorization not implemented for the parameters")
 	if (log.p) p <- exp(p)
 	if(!lower.tail) p <- 1 - p
-	q <- function(p) suppressWarnings(qfinder("ls", p, list(N=N, alpha=alpha)))
-	y <- sapply(p, q)
+  y <- rep(0, length(p))
+  y[1] <- suppressWarnings(qfinder(dls, p[i], list(N=N, alpha=alpha), 0))
+  for (i in 2:length(p))
+    y[i] <- suppressWarnings(qfinder(dls, p[i], list(N=N, alpha=alpha), y[i-1]))
+#	q <- function(p) suppressWarnings(qfinder(dls, p, list(N=N, alpha=alpha)))
+#	y <- sapply(p, q)
 	if(any(is.nan(y))) warning("NaNs produced")
 	return(y)
 }
--- R/utils.R
+++ R/utils.R
@@ -4,29 +4,29 @@ is.wholenumber	<-function(x, tol = .Machine$double.eps^0.5){
 
 # bissection algorith for finding the quantile of a *discrete*
 # and *unbounded* probability distribution function
-qfinder <- function(dist, want, coef) {
-	if (any(sapply(coef, length) > 1)) stop("Vectorization not implemented for the parameters")
-	f <- get(paste("d",dist, sep=""), mode="function")
-	# "phase 0": are the params invalid? is 0 overshooting? Is q = Inf?
+qfinder <- function(dist, want, coef, init = 0) {
+#	if (any(sapply(coef, length) > 1)) stop("Vectorization not implemented for the parameters")
+#	f <- get(paste("d",dist, sep=""), mode="function")
+	# "phase 0": are the params invalid? is "init" overshooting? Is q = Inf?
 	if (want < 0) return (0);
-    if (want >= 0.999999999999999999) return (Inf); 
-	q0 <- do.call(f, c(x=0, coef))
-	if (!is.nan(q0)) if (q0 >= want) return (0);
-	q1 <- do.call(f, c(x=1, coef))
-	if (is.nan(q1)) return (NaN);
-	if (q0 + q1 >= want) return (1);
+  if (want >= 0.999999999999999999) return (Inf); 
+	q0 <- do.call(dist, c(x=init, coef))
+	if (is.nan(q0)) return (NaN);
+	if (q0 >= want) return (init);
 	# phase 1: double the guess until you overshoot
-	guess <- 2
-	last <- 0
-  cum <- 0
+  step <- 1
+	guess <- init+1
+	last <- init
+  cum <- q0
 	repeat {
-		my.q <- do.call(f, c(list(x=last:guess), coef))
+		my.q <- do.call(dist, c(list(x=last:guess), coef))
     my.sq <- sum(my.q)
 		if (cum + my.sq > want) break;
 		# Is there a bug in pdist?
 		if (my.sq < 1e-8) stop ("quantile function did not converge!")
 		last <- guess+1
-		guess <- 2*guess
+    step <- step * 2
+		guess <- guess + step
     cum <- cum + my.sq
 	}
 	# Phase 2: exhaustive examine last interval
