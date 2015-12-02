a0bab0c77ecd89868cf47c7447a37a17767a083d
Andre Chalom <andrechalom@gmail.com>
2015-12-02 13:14:52 -0200

    Improved method for qmzsm

--- R/qmzsm.R
+++ R/qmzsm.R
@@ -1,14 +1,10 @@
 qmzsm<-function(p, J, theta, lower.tail = TRUE, log.p = FALSE){
-  op <- rank(p, ties.method="max")
-  p <- sort(p)
   if(length(J) > 1 | length(theta) > 1) stop("Vectorization not implemented for the parameters")
+  if (!all(is.finite(c(J, theta))) | theta <= 0 | J <= 0)
+	  return(rep(NaN, length(p)))
 	if (log.p) p <- exp(p)
 	if(!lower.tail) p <- 1 - p
-  y <- c()
-  y[1] <- suppressWarnings(qfinder(dmzsm, p[1], list(J=J, theta=theta), 0))
-  if(length(p) > 1)
-    for (i in 2:length(p))
-      y[i] <- suppressWarnings(qfinder(dmzsm, p[i], list(J=J, theta=theta), y[i-1]))
-	if(any(is.nan(y))) warning("NaNs produced")
-	return(y[op])
+  Y <- 1:J
+  X <- pmzsm(Y, theta=theta, J=J)
+  approx(X,Y,xout=p,method="constant",f=0,yleft=1, yright=J)$y
 }
