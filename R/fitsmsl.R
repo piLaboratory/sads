01b06dd08cecde49e4a569696fc5ba5c83197519
Andre Chalom <andrechalom@gmail.com>
2015-12-22 17:47:49 -0200

    Fixing identation

--- R/fitsmsl.R
+++ R/fitsmsl.R
@@ -1,22 +1,22 @@
 fitsmsl <- function(x, trunc = NaN, start.value, ...){
-  dots <- list(...)
-  if (!is.nan(trunc)){
-    warning("trunc argument for dsmsl is meaningless. Read the manual for details")
-  }
-  if (missing(start.value)){
-    n <-length(x); eps <- min(x)
-    dL <- function(delta) n*(1-delta*eps)/(delta*(delta*eps-log(delta*eps)-1)) - sum( x/(1-delta*x))
-    deltahat <- uniroot( dL, interval = c(1e-8, 0.9999/max(x)) )$root
-  } else{
-    deltahat <- start.value[1]
-  }
-  if(!"method" %in% names(dots)){
-    dots$method <- "Brent"
-    if(!"lower" %in% names(dots)) dots$lower=list(delta=1e-16)
-    if(!"upper" %in% names(dots)) dots$upper=list(delta=(1-1e-14)/max(x))
-  }
-  LL <- function(delta, epsilon) -sum(dsmsl(x, delta=delta, epsilon=epsilon, log = TRUE))
-  result <- do.call("mle2", c(list(LL, start = list(delta = deltahat), fixed = list(epsilon = min(x)), data = list(x = x)), dots))
-  new("fitsad", result, sad = "smsl", distr = distr.depr, trunc = NaN)
+    dots <- list(...)
+    if (!is.nan(trunc)){
+        warning("trunc argument for dsmsl is meaningless. Read the manual for details")
+    }
+    if (missing(start.value)){
+        n <-length(x); eps <- min(x)
+        dL <- function(delta) n*(1-delta*eps)/(delta*(delta*eps-log(delta*eps)-1)) - sum( x/(1-delta*x))
+        deltahat <- uniroot( dL, interval = c(1e-8, 0.9999/max(x)) )$root
+    } else{
+        deltahat <- start.value[1]
+    }
+    if(!"method" %in% names(dots)){
+        dots$method <- "Brent"
+        if(!"lower" %in% names(dots)) dots$lower=list(delta=1e-16)
+        if(!"upper" %in% names(dots)) dots$upper=list(delta=(1-1e-14)/max(x))
+    }
+    LL <- function(delta, epsilon) -sum(dsmsl(x, delta=delta, epsilon=epsilon, log = TRUE))
+    result <- do.call("mle2", c(list(LL, start = list(delta = deltahat), fixed = list(epsilon = min(x)), data = list(x = x)), dots))
+    new("fitsad", result, sad = "smsl", distr = distr.depr, trunc = NaN)
 }
 
