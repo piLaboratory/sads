d72951568f4c5a7528b6b131194159f6420d8b9a
piklprado <piklprado@gmail.com>
2017-09-08 20:31:20 -0300

    fitpareto with two free parameters

--- R/fitpareto.R
+++ R/fitpareto.R
@@ -15,9 +15,11 @@ fitpareto <- function(x, trunc, start.value, upper = 20, ...){
     LL <- function(shape, scale) -sum(dtrunc("pareto", x = x,
                                              coef = list(shape = shape, scale = scale), trunc = trunc, log = TRUE))
   }  
-  result <- do.call("mle2", c(list(LL, start = list(shape = alpha, scale=min(x)), fixed=list(scale=min(x)),
-                 data = list(x = x), method = "Brent", lower = 0, upper = upper), dots))
-  if(abs(as.numeric(result@coef) - upper) < 0.001) 
+  ## result <- do.call("mle2", c(list(LL, start = list(shape = alpha, scale=min(x)), fixed=list(scale=min(x)),
+  ##                data = list(x = x), method = "Brent", lower = 0, upper = upper), dots))
+  result <- do.call("mle2", c(list(LL, start = list(shape = alpha, scale=min(x)),
+                                   data = list(x = x), method = "L-BFGS-B", upper= c(shape = upper, scale=min(x))), dots))
+  if(abs(as.numeric(result@coef[1]) - upper) < 0.001) 
     warning("mle equal to upper bound provided. \n Try value for the 'upper' argument")
   new("fitsad", result, sad="pareto", distr = distr.depr, trunc = ifelse(missing(trunc), NaN, trunc)) 
 }
