5aca7012d7cad0f40d8315d3ecd38d1a9711b2da
piklprado <paulo@PI.localdomain>
2019-12-08 09:48:46 -0300

    to avoid warning on class slot fo fitsadC
    Warning: classes de slot não definidas na definição de ""fitsadC"": hist(class "histogram")

--- R/sads-classes.R
+++ R/sads-classes.R
@@ -21,7 +21,6 @@ setClass("rad", contains="data.frame", validity = function(object) {
 
 setClass("fitsad", contains="mle2", slots=c(sad="character", distr="character", trunc="numeric"))
 setClass("fitrad", contains="mle2", slots=c(rad="character", distr="character", trunc="numeric", rad.tab="rad"))
-setClass("fitsadC", contains="mle2", slots=c(sad="character", trunc="numeric", hist="histogram"))
 
 distr.depr <- "The 'distr' slot of fitrad and fitsad objects have been deprecated. Please see ?distr"
 
@@ -36,5 +35,5 @@ setClass("summary.sads", contains="summary.mle2", slots=c(fixed="numeric"))
 setClass("likelregions", contains="list", slots=c(names="character", ratio="numeric"))
 setClass("histogram", contains = "list") 
 
-
+setClass("fitsadC", contains="mle2", slots=c(sad="character", trunc="numeric", hist="histogram"))
 
